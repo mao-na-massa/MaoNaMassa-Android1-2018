0c3ca9560ee39f54ad73db7297ca045a8865c8f3
FL-SS-LAB3-19\Aluno <alessandraoitnb@gmail.com>
2018-06-16 12:05:55 -0300

    no message

---
+++ app/src/main/java/com/example/guilhermehayashi/maonamassabasico1/network/PetAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.guilhermehayashi.maonamassabasico1.network
+
+/**
+ * Created by Aluno on 16/06/2018.
+ */
+
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.guilhermehayashi.maonamassabasico1.R
+import com.example.guilhermehayashi.maonamassabasico1.network.Pet
+
+class PetAdapter(var pets: MutableList<Pet>, var context: Context): RecyclerView.Adapter<PetAdapter.PetViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetViewHolder {
+        return PetViewHolder(LayoutInflater.from(context).inflate(R.layout.petviewholder, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return pets.count()
+    }
+
+    override fun onBindViewHolder(holder: PetViewHolder, position: Int) {
+        holder.configurar(pets[position])
+    }
+
+    class PetViewHolder(var view: View): RecyclerView.ViewHolder(view) {
+
+        var nomeTextView: TextView? = null
+
+        init {
+            //nomeTextView = view.findViewById(R.id.nomeTextView)
+        }
+
+        fun configurar(pet: Pet) {
+            nomeTextView?.text = pet.nome
+        }
+
+    }
+
+}
