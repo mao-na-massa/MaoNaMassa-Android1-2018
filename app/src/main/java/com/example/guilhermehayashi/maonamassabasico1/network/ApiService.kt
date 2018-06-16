c8a425492eb946fc1df1fa21222cc71f9c57d432
FL-SS-LAB3-30\Aluno <coliv.lucas@gmail.com>
2018-06-16 12:00:49 -0300

    no message

---
+++ app/src/main/java/com/example/guilhermehayashi/maonamassabasico1/network/ApiService.kt
@@ -0,0 +1,27 @@
+package com.example.guilhermehayashi.maonamassabasico1.network
+
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.GET
+import rx.Observable
+
+public interface ApiService{
+    @GET(value="pets")
+    fun getPets(): Observable<ApiPetsResponse>
+
+}
+
+class ApiPetsResponse(
+        @SerializedName(value = "count")
+        @Expose var count:Int,
+
+
+        @SerializedName(value = "results")
+        @Expose var results:List<Pet>
+)
+
+class Pet (
+        @SerializedName (value="nome")
+        @Expose var nome:String
+)
